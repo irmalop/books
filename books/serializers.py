82196b714e1c3b7cefb73c6e32d6512fe4e0661c
irmalop <ada_wit18@hackademy.mx>
2022-03-05 12:29:05 -0600

    Books app, first commit

---
+++ books/serializers.py
@@ -0,0 +1,18 @@
+from dataclasses import field
+from rest_framework import serializers
+from books.models import Author, Book
+
+class  AuthorSerializer(serializers.ModelSerializer):
+    class  Meta:
+        model = Author
+        fields = ('id','first_name', 'last_name',)
+
+class  BookSerializer(serializers.ModelSerializer):
+    class  Meta:
+        model = Book
+        fields = ('__all__')
+    
+    def to_representation(self, instance):
+        response=super().to_representation(instance)
+        response['author']=AuthorSerializer(instance.author).data
+        return response
